b1f9106cef33b9f38821c189cd59abf06e6cd080
ezntek <eason@ezntek.com>
2024-07-27 14:42:08 +0800

    clean unused file + add license notice

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<!--
+Copyright (c) Eason Qin, 2024.
+
+This source code form is licensed under the MIT/Expat license.
+Read LICENSE.md at the root of the project to learn more.
+-->
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 </script>
